41d462ead2264d42d16e999e17d29defd1ec5083
mse11 <michal.sestrienka@gmail.com>
2019-08-15 15:18:54 +0200

    add Wes

--- wes/error.py
+++ wes/error.py
@@ -1,12 +1,33 @@
 from elasticsearch import Elasticsearch
 
+# elasticsearch - base
+from elasticsearch.exceptions import ImproperlyConfigured as WesImproperlyConfigured
+from elasticsearch.exceptions import SerializationError as WesSerializationError
+
+
 # elasticsearch - ConnectionError
+from elasticsearch.exceptions import ConnectionError
+
 from elasticsearch.exceptions import ConnectionTimeout as WesConnectionTimeout
 from elasticsearch.exceptions import SSLError as WesSSLError
 
 # elasticsearch - TransportError
-from elasticsearch.exceptions import RequestError as WesSSLError    # 400 status code
-from elasticsearch.exceptions import ConflictError as WesSSLError   # 409 status code
-from elasticsearch.exceptions import NotFoundError as WesSSLError   # 404 status code
+from elasticsearch.exceptions import TransportError
 
-# wes - placeholder ???
+from elasticsearch.exceptions import RequestError as WesRequestError    # 400 status code
+from elasticsearch.exceptions import ConflictError as WesConflictError   # 409 status code
+from elasticsearch.exceptions import NotFoundError as WesNotFoundError   # 404 status code
+
+# wes - placeholder - will I need it ???
+
+def err_2_string(e) -> str:
+    if isinstance(e, TransportError):
+        return f"{e.status_code} - {e.error} - {e.info} "
+    elif isinstance(e, ConnectionError):
+        return f"{e.info}"
+    elif isinstance(e, WesSerializationError):
+        return "Serialization error"
+    elif isinstance(e, WesImproperlyConfigured):
+        return "Improperly configured"
+    else:
+        raise("Unknow ...")
--- wes/wes.py
+++ wes/wes.py
@@ -1,5 +1,4 @@
 from elasticsearch import Elasticsearch
-from elasticsearch.exceptions import RequestError
 
 from error import *
 from log import *
@@ -9,16 +8,50 @@ class Wes:
         # self.es = Elasticsearch(HOST="http://localhost", PORT=9200)  # remote instance
         self.es = Elasticsearch()  # local instance
 
-    def create_indice(self, indice_name):
+    def ind_create(self, index) -> bool:
         try:
-            # rc = es.indices.create(index=indice_name, ignore=400)
-            rc = self.es.indices.create(index=indice_name)
+            rc = self.es.indices.create(index)
             LOG_OK(rc)
             # [DOCKER] {"type": "server", "timestamp": "2019-08-15T07:48:18,679+0000", "level": "INFO", "component": "o.e.c.m.MetaDataCreateIndexService", "cluster.name": "docker-cluster", "node.name": "9ed342ffe07d", "cluster.uuid": "481GmZMJSHu9Y6E-_7gqxQ", "node.id": "RoqxLVUpRy24ce_D3dRnKA",  "message": "[first_index] creating index, cause [api], templates [], shards [1]/[1], mappings []"  }
             # [PYTHON] {'acknowledged': True, 'shards_acknowledged': True, 'index': 'first_index'}
-        except RequestError as e:
-            LOG_ERR(e.error)
+            return True
+
+        except Exception as e:  # TODO is this ok???
+            LOG_ERR(err_2_string(e))
+            return False
+
+    def ind_exist(self, index, params=None) -> bool:
+        rc = self.es.indices.exists(index)
+        LOG(index + " " + str(rc))
+        return rc;
+
+    def ind_delete(self, indice_name) -> bool:
+        try:
+            rc = self.es.indices.delete(index=indice_name)
+            LOG(indice_name + " " + str(rc))
+            return True;
+        except Exception as e:  # TODO is this ok???
+            LOG_ERR(err_2_string(e))
+            return False
+
+
+
+############################################################################################
+# TEST
+############################################################################################
+
+class TestWes(unittest.TestCase):
+    def test_basic(self):
+        wes = Wes()
+        ind_str = "first_pooooooooooooooo"
+        wes.ind_delete(ind_str)
+        wes.ind_create(ind_str)
+        wes.ind_create(ind_str)
+        wes.ind_exist(ind_str)
+        wes.ind_delete(ind_str)
+        wes.ind_exist(ind_str)
+        wes.ind_delete(ind_str)
 
 if __name__ == '__main__':
-    wes = Wes()
-    wes.create_indice("first_index")
+    # unittest.main() run all test (imported too) :(
+    unittest.main(TestWes())
