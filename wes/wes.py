b2e06aaa38444173516be596c7ed803988f11685
mse11 <michal.sestrienka@gmail.com>
2019-08-16 23:27:36 +0200

    add ind_flush(), ind_flush_search(), ind_refresh(), ind_refresh_result(), doc_search(), doc_search_result()

--- wes/wes.py
+++ wes/wes.py
@@ -6,11 +6,14 @@ from log import *
 
 class WesDefs():
     # operation
-    OP_IND_CREATE = "OP_IND_CREATE"
-    OP_IND_EXIST  = "OP_IND_EXIST "
-    OP_IND_DELETE = "OP_IND_DELETE"
-    OP_DOC_ADD_UP = "OP_DOC_ADDUP "
-    OP_DOC_GET    = "OP_DOC_GET   "
+    OP_IND_CREATE   = "OP_IND_CREATE "
+    OP_IND_FLUSH    = "OP_IND_FLUSH  "
+    OP_IND_REFRESH  = "OP_IND_REFRESH"
+    OP_IND_EXIST    = "OP_IND_EXIST  "
+    OP_IND_DELETE   = "OP_IND_DELETE "
+    OP_DOC_ADD_UP   = "OP_DOC_ADDUP  "
+    OP_DOC_GET      = "OP_DOC_GET    "
+    OP_DOC_SEARCH   = "OP_DOC_SEARCH "
 
     # RC - 3 codes
     # - maybe useful later (low level could detect problem in data)
@@ -107,6 +110,56 @@ class Wes(WesDefs):
             raise ValueError(f"{Wes.OP_IND_DELETE} unknown status - {status}")
 
     @query_params(
+        "allow_no_indices",
+        "expand_wildcards",
+        "force",
+        "ignore_unavailable",
+        "wait_if_ongoing",
+    )
+    def ind_flush(self, index=None, params=None):
+        try:
+            rc = self.es.indices.flush(index=index, params=params)
+            WES_DB_OK(Wes.OP_IND_FLUSH, rc)
+            return (Wes.RC_OK, rc)
+        except Exception as e:
+            WES_DB_ERR(Wes.OP_IND_FLUSH, e)
+            return (Wes.RC_EXCE, e)
+
+    def ind_flush_result(self, index, rc: tuple):
+        status, rc = rc
+        if status == Wes.RC_OK:
+            WES_RC_OK(Wes.OP_IND_FLUSH, f"KEY[{index}] {str(rc)}")
+        elif status == Wes.RC_NOK:
+            assert("not implemented") # TODO RC - 3 codes
+        elif status == Wes.RC_EXCE:
+            WES_RC_NOK(Wes.OP_IND_FLUSH, rc)
+        else:
+            raise ValueError(f"{Wes.OP_IND_FLUSH} unknown status - {status}")
+
+    @query_params("allow_no_indices",
+                  "expand_wildcards",
+                  "ignore_unavailable")
+    def ind_refresh(self, index=None, params=None):
+        try:
+            rc = self.es.indices.refresh(index=index, params=params)
+            WES_DB_OK(Wes.OP_IND_REFRESH, rc)
+            return (Wes.RC_OK, rc)
+        except Exception as e:
+            WES_DB_ERR(Wes.OP_IND_REFRESH, e)
+            return (Wes.RC_EXCE, e)
+
+    def ind_refresh_result(self, index, rc: tuple):
+        status, rc = rc
+        if status == Wes.RC_OK:
+            WES_RC_OK(Wes.OP_IND_REFRESH, f"KEY[{index}] {str(rc)}")
+        elif status == Wes.RC_NOK:
+            assert("not implemented") # TODO RC - 3 codes
+        elif status == Wes.RC_EXCE:
+            WES_RC_NOK(Wes.OP_IND_REFRESH, rc)
+        else:
+            raise ValueError(f"{Wes.OP_IND_REFRESH} unknown status - {status}")
+
+    @query_params(
         "if_seq_no",
         "if_primary_term",
         "op_type",
@@ -178,6 +231,72 @@ class Wes(WesDefs):
         else:
             raise ValueError(f"{Wes.OP_DOC_GET} unknown status - {status}")
 
+    @query_params(
+        "_source",
+        "_source_exclude",
+        "_source_excludes",
+        "_source_include",
+        "_source_includes",
+        "allow_no_indices",
+        "allow_partial_search_results",
+        "analyze_wildcard",
+        "analyzer",
+        "batched_reduce_size",
+        "ccs_minimize_roundtrips",
+        "default_operator",
+        "df",
+        "docvalue_fields",
+        "expand_wildcards",
+        "explain",
+        "from_",
+        "ignore_throttled",
+        "ignore_unavailable",
+        "lenient",
+        "max_concurrent_shard_requests",
+        "pre_filter_shard_size",
+        "preference",
+        "q",
+        "rest_total_hits_as_int",
+        "request_cache",
+        "routing",
+        "scroll",
+        "search_type",
+        "seq_no_primary_term",
+        "size",
+        "sort",
+        "stats",
+        "stored_fields",
+        "suggest_field",
+        "suggest_mode",
+        "suggest_size",
+        "suggest_text",
+        "terminate_after",
+        "timeout",
+        "track_scores",
+        "track_total_hits",
+        "typed_keys",
+        "version",
+    )
+    def doc_search(self, index=None, body=None, params=None):
+        try:
+            rc = self.es.search(index=index, body=body, params=params)
+            WES_DB_OK(Wes.OP_DOC_SEARCH, rc)
+            return (Wes.RC_OK, rc)
+        except Exception as e:
+            WES_DB_ERR(Wes.OP_DOC_SEARCH, e)
+            return (Wes.RC_EXCE, e)
+
+    def doc_search_result(self, rc: tuple):
+        status, rc = rc
+        if status == Wes.RC_OK:
+            WES_RC_OK(Wes.OP_DOC_SEARCH, f"NB REC[{rc['hits']['total']['value']} <-> {rc['hits']['hits']}")
+        elif status == Wes.RC_NOK:
+            assert("not implemented") # TODO RC - 3 codes
+        elif status == Wes.RC_EXCE:
+            WES_RC_NOK(Wes.OP_DOC_SEARCH, rc)
+        else:
+            raise ValueError(f"{Wes.OP_DOC_SEARCH} unknown status - {status}")
+
 
 ############################################################################################
 # TEST
@@ -204,9 +323,9 @@ class TestWes(unittest.TestCase):
         wes.ind_exist_result(ind_str, wes.ind_exist(ind_str))
 
 
-        doc1 = {"city": "Bratislava1", "coutry": "slovakia1"}
-        doc2 = {"city": "Bratislava2", "coutry": "slovakia2"}
-        doc3 = {"city": "Bratislava3", "coutry": "slovakia3"}
+        doc1 = {"city": "Bratislava1", "country": "slovakia"}
+        doc2 = {"city": "Bratislava2", "country": "SLOVAKIA2"}
+        doc3 = {"city": "Bratislava3", "country": "slovakia"}
 
         # TODO petee explain success priority???
         # 1. exception
@@ -223,7 +342,14 @@ class TestWes(unittest.TestCase):
         wes.doc_get_result(wes.doc_get(ind_str, 3, doc_type="any"))  # MSE_NOTES: 'found': True,                 '_seq_no': 2,  '_source': {'city': 'Bratislava1', 'coutry': 'slovakia2'}
         wes.doc_get_result(wes.doc_get(ind_str, 9, doc_type="any"))  # MSE_NOTES:  WesNotFoundError !!!
 
-        wes.doc_addup(ind_str, doc3, doc_type="any", id=3)  # MSE_NOTES: 'result': 'updated' '_seq_no': 4
+        wes.doc_addup_result(wes.doc_addup(ind_str, doc3, doc_type="any", id=3))  # MSE_NOTES: 'result': 'updated' '_seq_no': 4
+
+        wes.ind_flush_result("_all", wes.ind_flush(index="_all", wait_if_ongoing=True))
+        wes.ind_refresh_result("_all", wes.ind_refresh(index="_all"))
+
+        wes.doc_search_result(wes.doc_search(index=ind_str, body={"query": {"match_all": {}}}))
+
+
 
 
 if __name__ == '__main__':
